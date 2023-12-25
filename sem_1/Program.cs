a7ec9fb105f37fa71343ce64a3c86cfb5f689b15
Eduard Minosyan <Edu.min@mail.ru>
2023-12-25 23:44:53 +0400

    added new information

--- sem_1/Program.cs
+++ sem_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// int number = 9; //number = 9
+// int result = number * number; // 81
+// Console.Write (result);
+
+// Console.Write ("Введите число: ");
+// int input = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine (input + 1000);
+// Команда "Console.ReadLine()" Позволяет вводить информацию пользователю.
+
