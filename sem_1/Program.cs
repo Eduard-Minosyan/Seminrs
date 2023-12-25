072073baa97730d43e83e3087d2f31d2a234c5d1
Eduard Minosyan <Edu.min@mail.ru>
2023-12-26 00:04:08 +0400

    added first task

--- sem_1/Program.cs
+++ sem_1/Program.cs
@@ -6,5 +6,26 @@
 // Console.Write ("Введите число: ");
 // int input = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine (input + 1000);
+
+
 // Команда "Console.ReadLine()" Позволяет вводить информацию пользователю.
+
+// (=) - Положить значение в переменную
+// (==) - Проверка а==5? (true, false)
+
+// ЗАДАЧА_1  Проверка является ли первое число квадратом второго!!
+Console.Write ("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber) // a является кв. от числа b
+{
+    Console.WriteLine ("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+}
+else // a не является кв. от числа b
+{
+    Console.WriteLine ("НЕТ, " + firstNumber + " НЕ является кв. от числа " + secondNumber);
+}
 
