ff7a68a20b8ff9de7b0d8a525fd60a0e6c6f11c0
Eduard Minosyan <Edu.min@mail.ru>
2023-12-26 00:34:33 +0400

    added task2

--- sem_1/Program.cs
+++ sem_1/Program.cs
@@ -11,21 +11,33 @@
 // Команда "Console.ReadLine()" Позволяет вводить информацию пользователю.
 
 // (=) - Положить значение в переменную
-// (==) - Проверка а==5? (true, false)
+// (==) - Проверка на равенство а==5? (true, false)
 
 // ЗАДАЧА_1  Проверка является ли первое число квадратом второго!!
-Console.Write ("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write ("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber == secondNumber * secondNumber) // a является кв. от числа b
+// if (firstNumber == secondNumber * secondNumber) // a является кв. от числа b
+// {
+//     Console.WriteLine("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+// }
+// else // a не является кв. от числа b
+// {
+//     Console.WriteLine("НЕТ, " + firstNumber + " НЕ является кв. от числа " + secondNumber);
+// }
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+//N = 4, 4 * (-1) = -4
+int negativeN = N * -1; // -4
+
+while (negativeN<=N)
 {
-    Console.WriteLine ("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+  Console.Write (negativeN + " "); 
+  negativeN++; // negativeN++ это (negativeN = negativen + 1)
 }
-else // a не является кв. от числа b
-{
-    Console.WriteLine ("НЕТ, " + firstNumber + " НЕ является кв. от числа " + secondNumber);
-}
+
 
