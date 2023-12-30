f504a592927e7ac0a00f7332d4f6e5b7284cde51
Eduard Minosyan <Edu.min@mail.ru>
2023-12-30 17:51:45 +0400

    add new task: sem_2

--- sem_2/Program.cs
+++ sem_2/Program.cs
@@ -40,20 +40,48 @@
 // 254 => 5 ^ 4 = 625
 // 617 => 1 ^ 7 = 1
 
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number >= 100 && number <=999)
+// {
+//     int secondDigit = number / 10 % 10; // 487 / 10 % 10 = 48 % 10 = 8
+//     int thirdDigit = number % 10; //487 % 10 = 7
+//     // Math.Pow (5, 2) => 5 в 2 степени => 25
+//     //int result = Convert.ToInt32(Math.Pow(secondDigit, thirdDigit)); // 1. Пример вывода с использованием Math.Pow
+//     int result = (int)Math.Pow(secondDigit, thirdDigit); // 2. Пример вывода с использованием Math.Pow// else // Число не трёхзначное
+//     //Console.WriteLine(result);
+// Console.WriteLine($"Число {number}: {secondDigit}^{thirdDigit} = {result}");
+// }
+
+// else
+// {
+//      Console.WriteLine("Число НЕ трёхзначное");
+// }
+
+//Семинар 2 Задача 3
+
+//Получите 3-ю цифру введенного числа слева (с начала)
+// 1234567 => 3
+// 7891 => 9
+// 32 => Третьей цифры нет
+
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int copyNumber = number;
 
-if (number >= 100 && number <=999)
+if (number < 100) // Третьей цифрын ет
 {
-    int secondDigit = number / 10 % 10; // 487 / 10 % 10 = 48 % 10 = 8
-    int thirdDigit = number % 10; //487 % 10 = 7
-    // Math.Pow (5, 2) => 5 в 2 степени => 25
-    //int result = Convert.ToInt32(Math.Pow(secondDigit, thirdDigit)); // 1. Пример вывода с использованием Math.Pow
-    int result = (int)Math.Pow(secondDigit, thirdDigit); // 2. Пример вывода с использованием Math.Pow// else // Число не трёхзначное
-    Console.WriteLine(result);
-}   Console.WriteLine($"Число  {number} => {result}");
+   Console.WriteLine("Третьей цифры нет :)");
+}
 
 else
 {
-     Console.WriteLine("Число НЕ трёхзначное");
+    while (number > 999)// Если в числе минимум 4 цифры делаем 
+    {
+        number /= 10; // number = number / 10
+        //a = a * 50: a *= 50
+        //a  = a - 450: a -= 450
+    }
+    Console.WriteLine($"Третья цифра от числа {copyNumber} = {number % 10}");
 }
